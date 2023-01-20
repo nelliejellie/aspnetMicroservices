b67d185de533d898e591b7eb5d9a97f9ea9572ec
Ewelike Emeka Nelson <emekaewelike@gmail.com>
2023-01-20 08:04:35 +0100

    contenarised discount api with docker compose

---
+++ src/Services/Discount/Discount.Api/Controllers/DiscountController.cs
@@ -0,0 +1,92 @@
+﻿using Discount.Api.Repository;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+using System;
+using Discount.Api.Entities;
+
+namespace Discount.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DiscountController : ControllerBase
+    {
+        private readonly IDiscountRepository _discountRepository;
+        
+
+        public DiscountController(IDiscountRepository discountRepository)
+        {
+            _discountRepository = discountRepository;
+        }
+
+        [HttpGet("{productName}", Name = "GetDiscount")]
+        public async Task<ActionResult<Coupon>> GetDiscount(string productName)
+        {
+            try
+            {
+                var discount = await _discountRepository.GetDiscount(productName);
+                return Ok(discount);
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+            
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Coupon>> CreateDiscount([FromBody] Coupon coupon)
+        {
+            
+            try
+            {
+                var discount = await _discountRepository.CreateDiscount(coupon);
+                return CreatedAtRoute("GetDiscount", new {productName = coupon.ProductName}, coupon);
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+        }
+
+        [HttpDelete]
+        public async Task<ActionResult<Coupon>> DeleteDiscount([FromBody] Coupon coupon)
+        {
+            try
+            {
+                var discount = await _discountRepository.DeleteDiscount(coupon.ProductName);
+                return Ok(new
+                {
+                    message = "deleted successfully",
+                    coupon = coupon,
+                });
+            }
+            catch (Exception ex)
+            {
+    
+                throw;
+            }
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<Coupon>> UpdateDiscount([FromBody] Coupon coupon)
+        {
+            try
+            {
+                var discount = await _discountRepository.UpdateDiscount(coupon);
+                return Ok(new
+                {
+                    message = discount,
+                    coupon = coupon,
+                });
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+    }
+}
---
+++ src/Services/Discount/Discount.Api/Repository/IDiscountRepositorycs.cs
@@ -0,0 +1,13 @@
+﻿using Discount.Api.Entities;
+using System.Threading.Tasks;
+
+namespace Discount.Api.Repository
+{
+    public interface IDiscountRepository
+    {
+        Task<Coupon> GetDiscount(string productName);
+        Task<bool> CreateDiscount(Coupon coupon);
+        Task<bool> UpdateDiscount(Coupon coupon);
+        Task<bool> DeleteDiscount(string productName);
+    }
+}
